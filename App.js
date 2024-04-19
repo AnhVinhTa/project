372b1409abca15010298ad5111c3793b8eec9f32
ngocmai1703 <doanngocmai5924@gmail.com>
2024-04-19 11:57:13 +0700

    first

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Bth4 from './src/Bth4';
+import Bth5 from './src/Bth5';
+import Bth6 from './src/Bth6';
+import Bth7 from './src/Bth7';
+import Bth8 from './src/Bth8';
+
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Bth8/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     height: 150,
+//     width: 150,
+//     alignItems: "center",
+//     backgroundColor: "aqua",
+//     justifyContent: "center"
+//   },
+//   Text: {
+//     fontSize: 20,
+//     fontWeight: "bold",
+//   }
+// });
